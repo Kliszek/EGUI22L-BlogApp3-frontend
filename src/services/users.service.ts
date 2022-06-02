59bb8076f436e2374c979c9b5a1ea9eeb3dca154
Jakub Kliszko <kuba.kliszko@gmail.com>
2022-06-02 21:22:16 +0200

    Added wrong credentials handling

--- src/services/users.service.ts
+++ src/services/users.service.ts
@@ -1,15 +1,23 @@
 import axios from "axios";
 import BaseHttpService from "./base-http.service";
 
-export default class UsersService extends BaseHttpService{
+export default class UsersService extends BaseHttpService {
   static async signin(username: string, password: string) {
-    const result = await axios.post(`${this.BASE_URL}/users/signin`, {
-      username,
-      password,
-    });
-    const accessToken = result.data.accessToken;
-    this.saveToken(accessToken);
-    return result.data.username;
+    const result: any = await axios
+      .post(`${this.BASE_URL}/users/signin`, {
+        username,
+        password,
+      })
+      .catch((error) => {
+        if (error.response.status === 403 || error.response.status === 404)
+          console.log("Wrong combination of username and password!");
+        else console.error(`ERROR: ${error.message}`);
+      });
+    if (result) {
+      const accessToken = result.data.accessToken;
+      this.saveToken(accessToken);
+    }
+    return;
   }
 
   static async signup(username: string, password: string) {
