1e83a6bfd676da314e24dc33d6bc29ef991eeac2
Jakub Kliszko <kuba.kliszko@gmail.com>
2022-06-03 22:09:43 +0200

    Made BlogsList more responsive

--- src/BlogsList/blogsList.tsx
+++ src/BlogsList/blogsList.tsx
@@ -16,25 +16,25 @@ export const BlogsList = () => {
       {blogs &&
         blogs.map((blog) => (
           <div key={blog.id} className="row justify-content-center mb-5">
-            <div className="col-10 col-md-8 col-lg-6 col-xl-8 text-start card shadow-sm">
+            <div className="col-10 col-lg-8 text-md-start card shadow-sm">
               <div className="card-body m-2">
-                <div className="d-flex justify-content-between">
+                <div className="d-flex flex-column flex-md-row justify-content-between">
                   <Link to={`/blogs/${blog.id}`}>
                     <h5 className="h3 d-inline-block ps-2 pe-4 py-1 fw-semibold border-1 border-bottom">{blog.title}</h5>
                   </Link>
                   <p>A blog by: {blog.ownerId}</p>
                 </div>
                 <div className="mt-4 d-flex row justify-content-end">
-                  <div className="start col-6">
+                  <div className="start col-12 col-md-6">
                     <h5 className="ps-2">
                       Latest entry:
                     </h5>
-                    <p className="text-nowrap overflow-hidden text-truncate bg-light rounded-3 p-3 pe-5 shadow-sm">
+                    <p className="text-nowrap overflow-hidden text-truncate text-start bg-light rounded-3 p-3 pe-5 shadow-sm">
                     { blog.blogEntryList.length > 0 && blog.blogEntryList[0].content}
                     { blog.blogEntryList.length === 0 && <>This blog has no entries yet.</> }
                     </p>
                   </div>
-                  <div className="align-self-center col-6 text-end">
+                  <div className="align-self-center col-12 col-md-6 text-md-end">
                     <button onClick={()=>navigate(`/blogs/${blog.id}`)} className="btn btn bg-primary text-white p-3 px-5">
                       Read this blog
                     </button>
